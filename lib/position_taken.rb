ecdbaba8b68e18a4eab233cc95d4020a1c0dd5eb
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-08-24 19:39:57 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, position, index)
+ puts "Please Enter A Position: "
+ user_position = gets.chomp
  
-  if board = [] , position = [] index = 0
+  if  position = [user_input] index = 0
     return false 
     
