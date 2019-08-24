66f74096d9f748a205bed761ad93b2f720afa5c3
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-08-24 19:48:15 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,14 +1,6 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
- puts "Please Enter A Position: "
- user_position = gets.chomp
-  if position == [user_position]
-    return FALSE
-    puts "Input Invalid, Postion Taken"
-  else 
-    return TRUE
-    puts "Input Valid."
-  end
+ 
 end
 
